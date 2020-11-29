f16eaab6b47a9dbc27fa68f39c8e9314854a6ec8
kaleemchughata007 <75196956+kaleemchughata007@users.noreply.github.com>
2020-11-29 12:31:53 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•KALEEM - CHUGHATA -------•◈•
+|__\~~) •||•KALEEM - CHUGHATA007---------------•◈•
+|__(-----\  •◈•------CHUGHATA-SHB--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  KALEEM CHUGHATA
+
+\033[1;96mFacebook  کلیم چوغظہ
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Kaleemchughata/zero
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mKaleem Chughata
+
+\003[1;96Facebook کلیم چوغظہ
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/KaleemChughata/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "kaleem"
+CorrectPassword = "chughata"
 
 loop = 'true'
 while (loop == 'true'):
